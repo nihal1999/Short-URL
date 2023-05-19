74bf519071e1bf4df59029ee845adef128a61328
nihal1999 <55922812+nihal1999@users.noreply.github.com>
2023-05-19 16:20:47 +0530

    Update settings.py

--- project/project/settings.py
+++ project/project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0zv_*7syqatp-xrshm-@dmu*vt0qb&&mxd)l&2hd@@nneb_upf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['cramain.pythonanywhere.com']
 
 
 # Application definition
